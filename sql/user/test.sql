a5f56ee21ee201d21f403b27f680bc681e9976b4
heokMO <gurah1220@gmail.com>
2023-02-13 15:36:57 +0900

    Create sql files for user table

---
+++ sql/user/test.sql
@@ -0,0 +1,27 @@
+# # 성공
+# insert into user
+# (account_id, password, nickname, email)
+# values
+#     ('test123', 'test', 'test', 'test@sdf.com');
+
+#
+# # account_id 실패
+# insert into user
+# (account_id, password, nickname, email)
+# values
+#     ('test', 'test', 'test', 'test@sdf.com');
+#
+# insert into user
+# (account_id, password, nickname, email)
+# values
+#     ('testㅆㄸ', 'test', 'test', 'test@sdf.com');
+#
+#
+# email 실패
+insert into user
+(account_id, password, nickname, email)
+values
+    ('test123', 'test', 'test', 'test');
+
+delete from user;
+
